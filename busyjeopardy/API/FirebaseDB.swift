203dc375315ed21d36b03becf0fd4a61fd5b6c7f
PJ Vea <pj.vea@busybusy.com>
2018-05-02 10:40:54 -0600

    Added updated Firebase listeners.

--- busyjeopardy/API/FirebaseDB.swift
+++ busyjeopardy/API/FirebaseDB.swift
@@ -11,6 +11,8 @@ import FirebaseDatabase
 struct Team {
     var id: String
     var name: String
+    var isConnected: Bool
+    var buzzerTapDate: Date?
 }
 
 final class FirebaseDB {
@@ -24,6 +26,21 @@ final class FirebaseDB {
         FirebaseDB.sharedInstance.reference.updateChildValues(resetDict)
     }
     
+    func getBuzzerWinner(completion: @escaping (_ winner: Team) -> Void) {
+        self.getTeamsListener { (teams) in
+            var teamsArray = teams
+            teamsArray.sort(by: {
+                if let date1 = $0.buzzerTapDate, let date2 = $1.buzzerTapDate, date1 > date2 {
+                    return true
+                }
+                return false
+            })
+            if let winningTeam = teamsArray.last, winningTeam.buzzerTapDate != nil {
+                completion(winningTeam)
+            }
+        }
+    }
+    
     func deleteTeam(name: String) {
         self.getTeams { (teams) in
             if let index = teams.index(where: { $0.name == name }) {
@@ -33,16 +50,62 @@ final class FirebaseDB {
         }
     }
     
+    func getIsConnected(completion: @escaping (_ isConnected: Bool) -> Void) {
+        guard let currentUser = FirebaseAuth.sharedInstance.firebaseAuth.currentUser else {
+            assertionFailure("currentUser must not be nil.")
+            return
+        }
+        self.reference.child("teams").child(currentUser.uid).child("is_connected").observe(.value) { (snapshot) in
+            if let isConnected = snapshot.value as? Bool {
+                completion(isConnected)
+            } else {
+                completion(false)
+            }
+        }
+    }
+    
     func getTeams(completion: @escaping (_ teams: [Team]) -> Void) {
+        self.reference.child("teams").observeSingleEvent(of: .value) { (snapshot) in
+            var teamsArray = [Team]()
+            for teams in snapshot.children.allObjects as! [DataSnapshot] {
+                if let teamObject = teams.value as? [String: AnyObject] {
+                    var name = ""
+                    var buzzerTap: Date? = nil
+                    var isConnected = false
+                    if let teamName = teamObject["team_name"] as? String {
+                        name = teamName
+                    }
+                    if let teamBuzzerTap = teamObject["buzzer_tap_date"] as? NSNumber {
+                        buzzerTap = Date(timeIntervalSince1970: teamBuzzerTap.doubleValue)
+                    }
+                    if let teamIsConnected = teamObject["is_connected"] as? Bool {
+                        isConnected = teamIsConnected
+                    }
+                    teamsArray.append(Team(id: teams.key, name: name, isConnected: isConnected, buzzerTapDate: buzzerTap))
+                }
+            }
+            completion(teamsArray)
+        }
+    }
+    
+    func getTeamsListener(completion: @escaping (_ teams: [Team]) -> Void) {
         self.reference.child("teams").observe(.value) { (snapshot) in
             var teamsArray = [Team]()
             for teams in snapshot.children.allObjects as! [DataSnapshot] {
                 if let teamObject = teams.value as? [String: AnyObject] {
                     var name = ""
+                    var buzzerTap: Date? = nil
+                    var isConnected = false
                     if let teamName = teamObject["team_name"] as? String {
                         name = teamName
                     }
-                    teamsArray.append(Team(id: teams.key, name: name))
+                    if let teamBuzzerTap = teamObject["buzzer_tap_date"] as? NSNumber {
+                        buzzerTap = Date(timeIntervalSince1970: teamBuzzerTap.doubleValue)
+                    }
+                    if let teamIsConnected = teamObject["is_connected"] as? Bool {
+                        isConnected = teamIsConnected
+                    }
+                    teamsArray.append(Team(id: teams.key, name: name, isConnected: isConnected, buzzerTapDate: buzzerTap))
                 }
             }
             completion(teamsArray)
